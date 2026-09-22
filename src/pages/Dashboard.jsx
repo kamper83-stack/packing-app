1072a774ecd8ee3f5869b27aea7659e7ca879265
kamper83-stack <kamper83@gmail.com>
2026-09-22 21:01:19 +0300

    refactor: remove flight search card

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -11,7 +11,6 @@ import {
   totalPassengers,
 } from "../utils/passengers";
 import DestinationPicker from "../components/DestinationPicker";
-import FlightSearch from "../components/FlightSearch";
 import Logo from "../components/Logo";
 import useDocumentTitle from "../utils/useDocumentTitle";
 
@@ -240,18 +239,6 @@ export default function Dashboard() {
                   />
                 </div>
               </div>
-
-              {/* Real round-trip flight search: pick an offer to auto-fill the
-                  trip's start (departure) and end (return) dates. */}
-              <FlightSearch
-                destination={destination}
-                departDate={startDate}
-                returnDate={endDate}
-                onSelectDates={({ departDate, returnDate }) => {
-                  if (departDate) setStartDate(departDate);
-                  if (returnDate) setEndDate(returnDate);
-                }}
-              />
 
               <div>
                 <label className="label">Airline</label>
